b395104506fc9180383d3db25480da93f97465cb
Mikolaj <m.iksrigz@gmail.com>
2025-06-16 14:42:59 +0200

    player and game class set up

--- script.js
+++ script.js
@@ -13,11 +13,34 @@ window.addEventListener("load", function () {
             this.game = game;
             this.width = 120;
             this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+        update() {
+            this.y += this.speedY;
+        }
+        draw(context) {
+            context.fillRect(this.x, this.y, this.width, this.height);
         }
     }
     class Enemy {}
     class Layer {}
     class Background {}
     class UI {}
-    class Game {}
+    class Game {
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+        }
+        update() {
+            this.player.update();
+        }
+        draw(context) {
+            this.player.draw(context);
+        }
+    }
+
+    const game = new Game(canvas.width, canvas.height);
 });
