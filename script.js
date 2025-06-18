8a11d24a24edd722f939b302e37b39bd5931044e
Mikolaj <m.iksrigz@gmail.com>
2025-06-18 19:20:09 +0200

    keybord input inplemented

--- script.js
+++ script.js
@@ -5,7 +5,24 @@ window.addEventListener("load", function () {
     canvas.width = 1500;
     canvas.height = 1500;
 
-    class InputHandler {}
+    class InputHandler {
+        constructor(game) {
+            this.game = game;
+            window.addEventListener("keydown", (e) => {
+                if (
+                    (e.key === "ArrowUp" || e.key === "ArrowDown") &&
+                    this.game.keys.indexOf(e.key) === -1
+                ) {
+                    this.game.keys.push(e.key);
+                }
+            });
+            window.addEventListener("keyup", (e) => {
+                if (this.game.keys.indexOf(e.key) > -1) {
+                    this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+                }
+            });
+        }
+    }
     class Projectile {}
     class Particle {}
     class Player {
@@ -16,8 +33,14 @@ window.addEventListener("load", function () {
             this.x = 20;
             this.y = 100;
             this.speedY = 0;
+            this.maxSpeed = 3;
         }
         update() {
+            if (this.game.keys.includes("ArrowUp"))
+                this.speedY = -this.maxSpeed;
+            else if (this.game.keys.includes("ArrowDown"))
+                this.speedY = this.maxSpeed;
+            else this.speedY = 0;
             this.y += this.speedY;
         }
         draw(context) {
@@ -33,6 +56,8 @@ window.addEventListener("load", function () {
             this.width = width;
             this.height = height;
             this.player = new Player(this);
+            this.input = new InputHandler(this);
+            this.keys = [];
         }
         update() {
             this.player.update();
